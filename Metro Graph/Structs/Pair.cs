f9084e1835ecd500258f3374847efd2a43617512
Oleg Putseyko <pts@feip.co>
2022-05-20 05:02:03 +1000

    Add Floyd-Warshall algorithm

---
+++ Metro Graph/Structs/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace Metro.Structs;
+
+public struct Pair<F, S>
+{
+    public F First { get; set; }
+    public S Second { get; set; }
+
+    public Pair(F first, S second)
+    {
+        this.First = first;
+        this.Second = second;
+    }
+}
+
